daf11c8aa1ed58715c3955c2944d709fea7979eb
Ming <ming.george.li@gmail.com>
2017-03-16 17:39:50 +0000

    finalise engine

--- app/engine/core.py
+++ app/engine/core.py
@@ -1,4 +1,4 @@
-#! ./venv/bin/python3 -m app.main
+#! ./venv/bin/python3 -m app.engine.core
 # -*- coding: utf-8 -*-
 
 import operator
@@ -7,67 +7,87 @@ import tensorflow as tf
 
 from app.settings import IMAGE_PATH, IMAGE_SHAPE
 from app.models.cnn import ConvolutionalNeuralNet
-from app.pipeline import datapipe
+from app.pipeline import data_pipe
 
 m = functools.reduce(operator.mul, IMAGE_SHAPE[:2], 1)
 d = 3
 n = 8
 
 g = tf.Graph()
-sess = tf.Session()
+# initiate a new graph, abandoning previous graph.
+with tf.Session().as_default() as sess:
 
-cnn = ConvolutionalNeuralNet(shape=(None, d, m))
+    cnn = ConvolutionalNeuralNet(shape=(None, d, m))
 
-x_placeholder, y_placeholder, keep_prob = cnn.x, cnn._y, cnn.keep_prob
-# (72, 128, 3)
-conv_layer_1 = cnn.add_conv_layer(x_placeholder, [[5, 5, 3, 12], [12]], func='sigmoid')
-# (72, 128, 3 * 12)
-conv_layer_2 = cnn.add_conv_layer(conv_layer_1, [[5, 5, 12, 24], [24]], func='relu')
-# (72, 128, 3 * 24)
-max_pool_1 = cnn.add_pooling_layer(conv_layer_2)
-# (36, 64, 3 * 24)
-conv_layer_3 = cnn.add_conv_layer(max_pool_1, [[5, 5, 24, 48], [48]], func='sigmoid')
-# (36, 64, 3 * 48)
-conv_layer_4 = cnn.add_conv_layer(conv_layer_3, [[5, 5, 48, 48], [48]], func='relu')
-# (36, 64, 3 * 48)
-max_pool_2 = cnn.add_pooling_layer(conv_layer_4)
-# (18, 32, 3 * 48)
-conv_layer_5 = cnn.add_conv_layer(max_pool_2, [[5, 5, 48, 96], [96]], func='sigmoid')
-# (18, 32, 3 * 96)
-conv_layer_6 = cnn.add_conv_layer(conv_layer_5, [[5, 5, 96, 96], [96]], func='relu')
-# (18, 32, 3 * 96)
-max_pool_3 = cnn.add_pooling_layer(conv_layer_6)
-# (9, 16, 3 * 96)
-fully_connected_layer_1 = cnn.add_dense_layer(
-                            max_pool_3,
-                            [[9 * 16 * 288, 2048], [2048], [-1, 9 * 16 * 288]],
-                            func='sigmoid'
-                            )
-fully_connected_layer_2 = cnn.add_dense_layer(
-                            fully_connected_layer_1,
-                            [[9 * 16 * 288, 1024], [1024], [-1, 9 * 16 * 288]],
-                            func='relu'
-                            )
-drop_out_layer_1 = cnn.add_drop_out_layer(fully_connected_layer_2)
-read_out = cnn.add_read_out_layer(drop_out_layer_1, [[1024, 8], [8]])
+    x_placeholder, y_placeholder, keep_prob = cnn.x, cnn._y, cnn.keep_prob
+    # (72, 128, 3)
+    conv_layer_1 = cnn.add_conv_layer(x_placeholder, [[5, 5, 3, 12], [12]], func='sigmoid')
+    # (72, 128, 3 * 12)
+    conv_layer_2 = cnn.add_conv_layer(conv_layer_1, [[5, 5, 12, 24], [24]], func='relu')
+    # (72, 128, 3 * 24)
+    max_pool_1 = cnn.add_pooling_layer(conv_layer_2)
+    # (36, 64, 3 * 24)
+    conv_layer_3 = cnn.add_conv_layer(max_pool_1, [[5, 5, 24, 48], [48]], func='sigmoid')
+    # (36, 64, 3 * 48)
+    conv_layer_4 = cnn.add_conv_layer(conv_layer_3, [[5, 5, 48, 48], [48]], func='relu')
+    # (36, 64, 3 * 48)
+    max_pool_2 = cnn.add_pooling_layer(conv_layer_4)
+    # (18, 32, 3 * 48)
+    conv_layer_5 = cnn.add_conv_layer(max_pool_2, [[5, 5, 48, 96], [96]], func='sigmoid')
+    # (18, 32, 3 * 96)
+    conv_layer_6 = cnn.add_conv_layer(conv_layer_5, [[5, 5, 96, 96], [96]], func='relu')
+    # (18, 32, 3 * 96)
+    max_pool_3 = cnn.add_pooling_layer(conv_layer_6)
+    # (9, 16, 3 * 96)
+    fully_connected_layer_1 = cnn.add_dense_layer(
+                                max_pool_3,
+                                [[9 * 16 * 288, 2048], [2048], [-1, 9 * 16 * 288]],
+                                func='sigmoid'
+                                )
+    fully_connected_layer_2 = cnn.add_dense_layer(
+                                fully_connected_layer_1,
+                                [[9 * 16 * 288, 1024], [1024], [-1, 9 * 16 * 288]],
+                                func='relu'
+                                )
+    drop_out_layer_1 = cnn.add_drop_out_layer(fully_connected_layer_2)
+    read_out = cnn.add_read_out_layer(drop_out_layer_1, [[1024, 8], [8]])
 
-# train
-cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=y_placeholder, logits=read_out)
-loss = tf.reduce_mean(cross_entropy)
-train_step = tf.train.AdamOptimizer().minimize(loss)
+    # train
+    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=y_placeholder, logits=read_out)
+    loss = tf.reduce_mean(cross_entropy)
+    train_step = tf.train.AdamOptimizer().minimize(loss)
 
-# eval
-correct_prediction = tf.equal(tf.argmax(read_out, 1), tf.argmax(y_placeholder, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    # eval
+    correct_prediction = tf.equal(tf.argmax(read_out, 1), tf.argmax(y_placeholder, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-initializer = tf.global_variables_initializer()
+    p = data_pipe(root_dir=IMAGE_PATH + 'train/', test_size=.1)
 
-input_image_directory = IMAGE_PATH +  'train/'
+def train(pipeline, optimiser, metric, loss):
+    """train neural network with batched data feed."""
+    train_image_batch, train_label_batch, valid_image_batch, valid_label_batch = pipeline
+    queue_initailizer = tf.train.start_queue_runners()
 
-p = data_pipe(input_image_directory, test_size=.1)
+    for n in range(10):
+
+        optimiser.run(feed_dict={
+                x_placeholder: train_image_batch.eval(),
+                y_placeholder: train_label_batch.eval(),
+                keep_prob: .5
+            })
+
+        if n % 5 == 0:
+            valid_accuracy, loss_score = sess.run([metric, loss], feed_dict={
+                        x_placeholder: valid_image_batch.eval(),
+                        y_placeholder: valid_label_batch.eval(),
+                        keep_prob: .5
+                    })
+            print("step {0}, validation accuracy {1:.4f}, loss {3:.4f}".
+                                        format(n, valid_accuracy, loss_score))
+
+    return None
 
 
-def optimise(pipeline):
-X_image_batch, X_label_batch, y_image_batch, y_label_batch
-
-optimiser.run(feed_dict={x: x_batch, y_: y_batch, keep_prob_1: drop_out[0]})
+with sess:
+    initializer = tf.global_variables_initializer()
+    train(p, train_step, accuracy, loss)
--- app/main.py
+++ app/main.py
@@ -5,7 +5,9 @@ import sys
 import tensorflow as tf
 import numpy as np
 
-from app.pipeline import datapipe
+from PIL import Image
+
+from app.pipeline import data_pipe
 from app.settings import IMAGE_PATH
 
 EVAL = True if 'EVAL' in map(str.upper, sys.argv[1:]) else False
@@ -13,11 +15,7 @@ EVAL = True if 'EVAL' in map(str.upper, sys.argv[1:]) else False
 if __name__ == '__main__':
 
     with tf.Session().as_default():
-        test_image_folder = IMAGE_PATH + 'train/'
+        test_image_folder = IMAGE_PATH + 'train/LAG'
         image_batch, label_batch, _, _ = \
             data_pipe(test_image_folder, test_size=.1)
         queue_initailizer = tf.train.start_queue_runners()
-        label_batch = label_batch.eval()
-        image = image_batch.eval()
-        print(image[0])
-        print('Hello, World')
