f668decfd91fe6afc9499015c0d1bd5ef300f79b
Ming <ming.george.li@gmail.com>
2017-03-19 16:39:33 +0000

    minor change

--- app/engine/core.py
+++ app/engine/core.py
@@ -83,12 +83,8 @@ init_op = tf.group(
 if not EVAL:
     with sess:
         sess.run(init_op)
-        coord = tf.train.Coordinator()
-        threads = tf.train.start_queue_runners(coord=coord)
         train(MAX_STEPS, sess, x, _y, keep_prob, train_image_batch, train_label_batch,
         valid_image_batch, valid_label_batch, train_step, accuracy, loss)
-        coord.request_stop()
-        coord.join(threads)
         save_session(sess, path=MODEL_PATH)
 elif EVAL:
     sess.run(init_op)
