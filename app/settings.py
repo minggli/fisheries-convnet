aea5a7ed78aeaad87ab5c72a590d08a9d0a2f56e
Ming <ming.george.li@gmail.com>
2017-03-20 22:13:35 +0000

    update params

--- app/settings.py
+++ app/settings.py
@@ -3,6 +3,6 @@
 MODEL_PATH = './trained_models/'
 IMAGE_PATH = './data/'
 IMAGE_SHAPE = (72, 128, 3)
-BATCH_SIZE = 200
-MAX_STEPS = 500
-ALPHA = 1e-2
+BATCH_SIZE = 64
+MAX_STEPS = 1000
+ALPHA = 1e-3
