d47bd96100a9a488e5ee55e2da5e73f8f0699e13
Ming <ming.george.li@gmail.com>
2017-03-21 13:18:20 +0000

    update tests

--- tests/test_onehotencoding.py
+++ tests/test_onehotencoding.py
@@ -12,24 +12,28 @@ train_file_array, train_label_array, valid_file_array, valid_label_array = \
         generate_data_skeleton(root_dir=IMAGE_PATH + 'train', valid_size=.2)
 valid_image_batch, valid_label_batch = \
         data_pipe(valid_file_array, valid_label_array, num_epochs=1, shuffle=False)
+train_image_batch, train_label_batch = \
+        data_pipe(train_file_array, train_label_array, num_epochs=1, shuffle=False)
 
 init_op = tf.group(
             tf.local_variables_initializer(), tf.global_variables_initializer())
+
 sess.run(init_op)
 
 @multi_threading
 def test_image():
-    whole_valid_images = list()
-    for _ in range(10):
+    whole_train_labels = list()
+    for _ in range(10000):
         try:
-            valid_image = sess.run(valid_image_batch)
-            # print(valid_image[155])
-            whole_valid_images.append(valid_image)
+            train_label = sess.run(train_label_batch)
+            whole_train_labels.append(train_label)
         except tf.errors.OutOfRangeError as e:
-            flattened = [piece for blk in whole_valid_images for piece in blk]
+            flattened = [piece for blk in whole_train_labels for piece in blk]
             break
 
-    print(flattened[-1])
+    output = {i: flattened[k] for k, i in enumerate(train_file_array)}
+    for key in output:
+        print('{0}: {1}'.format(key, output[key]))
 
 with sess:
     test_image()
