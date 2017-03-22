aee662fe0372a452828f8b4d5fa667af2c5af415
Ming <ming.george.li@gmail.com>
2017-03-22 12:09:20 +0000

    test predict

--- app/engine/vgg16.py
+++ app/engine/vgg16.py
@@ -117,20 +117,21 @@ elif EVAL:
                         tf.global_variables_initializer()
                     )
 
-    sess.run(init_op)
 
     with sess:
         # restore_session(sess, MODEL_PATH)
         eval_saver = tf.train.import_meta_graph(tf.train.latest_checkpoint(MODEL_PATH) + '.meta')
         eval_saver.restore(sess, tf.train.latest_checkpoint(MODEL_PATH))
         print('restore success.')
+        sess.run(init_op)
+        tf.start_queue_runners()
         # probs = predict(sess, x, keep_prob, logits, test_image_batch)
         complete_probs = list()
         for _ in range(50):
             try:
                 test_image = sess.run(test_image_batch)
                 probs = sess.run(tf.nn.softmax(logits),
-                                        feed_dict={x: test_image, keep_prob: 1.0})
+                        feed_dict={x: test_image, keep_prob: 1.0})
                 complete_probs.append(probs)
                 for i in probs:
                     print(i)
