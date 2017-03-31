1509fe5e1bd938557d8bc518e197ea181dec6e25
Ming <ming.george.li@gmail.com>
2017-03-31 20:42:03 +0100

    sampling

--- app/cv/sampling.py
+++ app/cv/sampling.py
@@ -41,13 +41,16 @@ def generate_sample_skeleton(synset_dict, sample_size):
         try:
             r = requests.get(BASE_URL.format(wnid),
                              allow_redirects=True,
-                             timeout=5
-                             )
+                             timeout=5)
         except requests.exceptions.ConnectionError:
             raise RuntimeError('no active Internet connection.')
         synset_urls.append(r.text.split('\r\n'))
-    unravelled = [url for nested_set in synset_urls for url in nested_set]
+
+    unravelled = [url for nested_set in synset_urls for url in nested_set
+                  if 'http' in url and '.' in url.split('/')[-1]]
+
     random.shuffle(unravelled)
+
     if 0 < sample_size <= 1:
         return random.sample(unravelled, int(sample_size * len(unravelled)))
     elif sample_size > 1:
@@ -57,17 +60,28 @@ def generate_sample_skeleton(synset_dict, sample_size):
             return unravelled
 
 
-def batch_retrieve(func, iterable):
+@timeit
+def batch_retrieve(func, iterable, path):
     """processing through iterable (e.g. list)"""
     from multiprocessing import Pool
+    from itertools import repeat
 
     with Pool(4) as p:
-        p.starmap(func, zip(iterable, CV_SAMPLE_PATH + 'neg'))
+        p.starmap(func, zip(iterable, repeat(path)))
 
 
 def retrieve_image(image_url, path):
     """download single image and save in path"""
-    r = requests.get(image_url, allow_redirects=True, timeout=5, stream=True)
+    try:
+        r = requests.get(image_url,
+                         allow_redirects=False,
+                         timeout=5,
+                         stream=True)
+    except (requests.exceptions.ConnectTimeout,
+            requests.exceptions.ConnectionError,
+            requests.exceptions.ReadTimeout,
+            requests.packages.urllib3.exceptions.ReadTimeoutError) as e:
+        return None
     code = r.status_code
     print(code, image_url, flush=True)
     if code == 200:
@@ -79,9 +93,15 @@ def retrieve_image(image_url, path):
             shutil.copyfileobj(r.raw, f)
 
 
-sample_pos = generate_sample_skeleton(synset_ids_pos, sample_size=50)
-sample_neg = generate_sample_skeleton(synset_ids_neg, sample_size=50)
+sample_pos = generate_sample_skeleton(synset_ids_pos, sample_size=1000)
+sample_neg = generate_sample_skeleton(synset_ids_neg, sample_size=5000)
+
 print(len(sample_pos))
 print(len(sample_neg))
-# retrieve_image(image_url=sample_neg[5], path=CV_SAMPLE_PATH + 'neg')
-batch_retrieve(func=retrieve_image, iterable=sample_neg)
+
+batch_retrieve(func=retrieve_image,
+               iterable=sample_neg,
+               path=CV_SAMPLE_PATH + 'neg')
+batch_retrieve(func=retrieve_image,
+               iterable=sample_pos,
+               path=CV_SAMPLE_PATH + 'pos')
