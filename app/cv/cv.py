115bdedd4bd14d9a66590b7e9fba3aed929eea91
Ming <ming.george.li@gmail.com>
2017-03-31 11:44:13 +0100

    add assets

--- app/cv/cv.py
+++ app/cv/cv.py
@@ -2,7 +2,10 @@
 """
 object detection module
 
-using OpenCV to detect the single specy of fish in each image
+using OpenCV to detect the single species of fish in each image
+
+Competition allows use of external data, ImageNet data will be used for object
+detection purpose for identify Region of Interest (ROI) containing fish.
 
 mechanics of Haar Cascade algorithm:
 http://docs.opencv.org/trunk/d7/d8b/tutorial_py_face_detection.html
@@ -12,3 +15,23 @@ http://docs.opencv.org/trunk/dc/d88/tutorial_traincascade.html
 """
 
 import cv2
+
+face_cascade = cv2.CascadeClassifier('app/cv/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('app/cv/haarcascade_eye.xml')
+
+img = cv2.imread('app/cv/face_detection.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+    for (ex, ey, ew, eh) in eyes:
+        cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+cv2.imshow('img', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
