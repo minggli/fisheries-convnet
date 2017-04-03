63d8582f7155bceba026e1fc8c3bc89a2779ddae
Ming <ming.george.li@gmail.com>
2017-04-03 15:51:08 +0100

    modify description to use bounding boxes

--- app/cv/description.py
+++ app/cv/description.py
@@ -15,27 +15,54 @@ test set.
 """
 
 import os
+import json
 import cv2
 
-from app.pipeline import folder_traverse, generate_data_skeleton
-from app.settings import CV_CROPPED_SAMPLE_PATH, CV_SAMPLE_PATH
-
-bb_file_array, _ = generate_data_skeleton(
-                        os.path.dirname(os.path.realpath(__file__)) + 'bb/')
+from app.pipeline import folder_traverse
+from app.settings import CV_SAMPLE_PATH
 
 
+def deserialize_json(rootdir, ext=('json')):
+    """concatenate and deserialize bounding boxes in json format"""
+    bb_file_structure = folder_traverse(rootdir, ext=ext)
+    fish_annotation = list()
+    for folder, filelist in bb_file_structure.items():
+        for filename in filelist:
+            with open(folder+filename) as f:
+                label = json.load(f)
+                fish_annotation.append(label)
+    # individual json object from nested lists
+    fish_annotation = {json_object['filename']: json_object for nested_list
+                       in fish_annotation for json_object in nested_list}
+    return fish_annotation
+
+
+# producing positive samples with required description format by OpenCV
+bb = deserialize_json(os.path.join(
+                 os.path.dirname(os.path.realpath(__file__)), 'bb/'))
 file_structure = folder_traverse(
-                os.path.join(os.path.realpath('.'), CV_SAMPLE_PATH + 'pos/'))
-
+                 os.path.join(os.path.realpath('.'), CV_SAMPLE_PATH + 'pos/'))
 f = open(os.path.dirname(os.path.realpath(__file__)) + '/positives.dat', 'w')
-
 for folder, filelist in file_structure.items():
     for filename in filelist:
-        h, w = cv2.imread(folder+filename, -1).shape[:2]
-        string = '{0}{1} 1 0 0 {2} {3}\n'.format(folder, filename, w, h)
-        f.write(string)
+        data = bb[filename]['annotations']
+        path_to_image = '{0}{1} '.format(folder, filename)
+        num_object = '{0}'.format(len(data))
+        boundingbox_template = ' {0} {1} {2} {3}'
+        bbs = ''
+        endline = '\n'
+        for i in range(int(num_object)):
+            x = str(int(data[i]['x']))
+            y = str(int(data[i]['y']))
+            height = str(int(data[i]['height']))
+            width = str(int(data[i]['width']))
+            bbs += boundingbox_template.format(x, y, width, height)
+        print_string = ''.join([path_to_image, num_object, bbs, endline])
+        f.write(print_string)
 f.close()
 
+
+# produce negative set...simply list of negative images location in abspath
 file_structure = folder_traverse(
                 os.path.join(os.path.realpath('.'), CV_SAMPLE_PATH + 'neg/'))
 f = open(os.path.dirname(os.path.realpath(__file__)) + '/negatives.dat', 'w')
