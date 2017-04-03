c492c2d76c592ee24e0df596e2cf7bb54a222480
Ming <ming.george.li@gmail.com>
2017-04-03 16:48:23 +0100

    positive vector downsize

--- app/cv/sampletrain.sh
+++ app/cv/sampletrain.sh
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 rm fishcascade/*
-opencv_createsamples -info positives.dat -vec pos.vec -w 45 -h 80
+opencv_createsamples -info positives.dat -vec pos.vec -w 40 -h 40 -num 3300
 opencv_traincascade -data fishcascade -vec pos.vec -bg negatives.dat \
-  -numStages 20 -minHitRate 0.999 -maxFalseAlarmRate 0.5 -numPos 10 \
-  -numNeg 300 -w 45 -h 80 -mode ALL -precalcValBufSize 1024\
+  -numStages 20 -minHitRate 0.999 -maxFalseAlarmRate 0.5 -numPos 3300 \
+  -numNeg 450 -w 40 -h 40 -mode ALL -precalcValBufSize 1024\
   -precalcIdxBufSize 1024
