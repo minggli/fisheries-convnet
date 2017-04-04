472cbfa7c556d967d2b12566f9ee100ad8974f68
Ming <ming.george.li@gmail.com>
2017-04-04 09:51:25 +0100

    amend sampletrain

--- app/cv/sampletrain.sh
+++ app/cv/sampletrain.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-opencv_createsamples -info positives.dat -vec pos.vec -w 40 -h 40 -num 1000
+opencv_createsamples -info positives.dat -vec pos.vec -w 40 -h 40 -num 3300
 opencv_traincascade -data fishcascade -vec pos.vec -bg negatives.dat \
-  -numStages 20 -minHitRate 0.99 -maxFalseAlarmRate 0.5 -numPos 1000 \
+  -numStages 20 -minHitRate 0.99 -maxFalseAlarmRate 0.5 -numPos 500 \
   -numNeg 400 -w 40 -h 40 -mode ALL -precalcValBufSize 1024\
   -precalcIdxBufSize 1024
