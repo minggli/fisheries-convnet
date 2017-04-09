d2d2ee721515a91ff4020559b41df7745346e344
Ming <ming.george.li@gmail.com>
2017-04-09 21:57:59 +0100

    modify test and sampletrain

--- app/cv/sampletrain.sh
+++ app/cv/sampletrain.sh
@@ -7,6 +7,6 @@ export SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $SCRIPT_DIR
 opencv_createsamples -info positives.dat -vec pos.vec -w 40 -h 40 -num 3300
 opencv_traincascade -data fishcascade -vec pos.vec -bg negatives.dat \
-  -numStages 20 -minHitRate 0.99 -maxFalseAlarmRate 0.5 -numPos 2500 \
+  -numStages 10 -minHitRate 0.99 -maxFalseAlarmRate 0.5 -numPos 2000 \
   -numNeg 3000 -w 40 -h 40 -mode ALL -precalcValBufSize 1024\
   -precalcIdxBufSize 1024
--- tests/test_cv.py
+++ tests/test_cv.py
@@ -4,25 +4,28 @@ test_cv
 
 using pre-trained front face and eye detector to identify a totally random face
 """
-
 import cv2
+import random
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+from app.settings import CV_SAMPLE_PATH, HAARCASCADE, HAARPARAMS
+from app.pipeline import generate_data_skeleton
+
+file_arary = generate_data_skeleton(CV_SAMPLE_PATH + 'pos')[0]
+
+cascade = cv2.CascadeClassifier(HAARCASCADE + 'cascade.xml')
 
 # reads in image in BGR rather than RGB format
-img = cv2.imread('face.jpg')
+img = cv2.imread(random.choice(file_arary), -1)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+fish = cascade.detectMultiScale(gray,
+                                minNeighbors=HAARPARAMS['minNeighbors'],
+                                minSize=HAARPARAMS['minSize'],
+                                maxSize=HAARPARAMS['maxSize']
+                                )
 
-for (x, y, w, h) in faces:
+for (x, y, w, h) in fish:
     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    for (ex, ey, ew, eh) in eyes:
-        cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
 
 cv2.imshow('img', img)
 cv2.waitKey(0)
